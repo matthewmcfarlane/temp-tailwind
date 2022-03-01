6bc3ed56e6876dad0df23921bdb674ef4b160e6e
Matthew <matthewmcfarlane1@outlook.com>
2022-03-01 13:44:00 +0000

    integration with initial proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-import Channelbar from './components/ChannelBar';
 import DashboardContainer from './components/DashboardContainer';
 import SideBar from './components/SideBar';
 import { useAuth0 } from '@auth0/auth0-react';
 import TopNavigation from './components/TopNavigation'
+import React, { useEffect, useState } from 'react';
+import Loading from './components/Loading';
+import BugTable from './components/BugContainer/BugTable';
 
 function App() {
 
-  const { isLoading, loginWithRedirect, isAuthenticated } = useAuth0();
+  const { isLoading, loginWithRedirect, isAuthenticated, user } = useAuth0();
+  const [foundUser, setFoundUser] = useState(null);
+
+  const baseURL = 'http://localhost:9090/users/';
+
+  const postUser = (payload) => {
+    return fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then(data => setFoundUser(data));
+  }
+  
+  const checkUserInDB = () => {
+
+    const userData = {
+      "auth0Sub": user.sub,
+      "name": user.name,
+      "nickname": user.nickname,
+      "email": user.email,
+      "role": user["http://demozero.net/roles"][0]
+    };
+
+    postUser(userData);
+  }
+
+  useEffect(() => {
+    if(isAuthenticated){
+      checkUserInDB();
+    }
+  }, [user])
 
 
-
-  if (isLoading) return <p>Loading....</p>
+  if (isLoading) return <Loading />
 
   return (
     !isAuthenticated &&(loginWithRedirect()),
@@ -18,7 +51,8 @@ function App() {
 <TopNavigation />
 <SideBar />
     <div className="">
-      <DashboardContainer />
+      {/* <DashboardContainer /> */}
+      <BugTable />
     </div>
   </>
   );
